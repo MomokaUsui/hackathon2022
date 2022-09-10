aa96149c8e30c822238d7ce95818d9d4baf9c407
momoyama <momongamomoyama@gmail.com>
2022-09-10 13:37:53 +0900

    image追加

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import obake1 from "../src/images/obake1.png";
+import obake2 from "../src/images/obake2.png";
+import obake3 from "../src/images/obake3.png";
 function Main() {
-  return <div>ここがMainページです</div>;
+  return (
+    <div>
+      ここがMainページです
+      <img src={obake1} className="w-20" />
+      <img src={obake2} className="w-20"/>
+      <img src={obake3} className="w-20"/>
+    </div>
+  );
 }
 
 export default Main;
