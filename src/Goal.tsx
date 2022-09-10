7450d5564da45abebabce7adc3349cc39ccd254a
toshiki19 <toshiki.u.19@gmail.com>
2022-09-10 15:32:17 +0900

    goalの背景追加

--- src/Goal.tsx
+++ src/Goal.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import apartment from "../src/images/apartment.jpeg"
 
 function Goal() {
+  //javascript
   return (
-    <div>
+    <div style={{backgroundImage: `url(${apartment} )`, backgroundSize: "cover", height: "100vh"}}>
       <div>Goal</div>
     </div>
   );
