5295291db7c951aeadefe922697ef78bed33a4fc
haa_hayato <gotopeco@gmail.com>
2022-09-11 22:29:28 +0900

    キャンセルボタン削除

--- src/Login.tsx
+++ src/Login.tsx
@@ -15,7 +15,7 @@ const Login: React.FC = () => {
   const [isCancelOpen, setCancelOpen] = useState(false)
   const [checkBox, setCheckBox] = useState(false);
   const [checkPolicy, setCheckPolicy] = useState(false)
-  const [count, setCount] = useState(0) //キャンセルボタン用
+  
   const newUserNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
   };
@@ -32,16 +32,12 @@ const Login: React.FC = () => {
     sessionStorage.clear()
   }, [])
 
-  useEffect(() => { //キャンセルボタン用
-    setCancelOpen(!isCancelOpen)
-    console.log("無限ループ")
-  }, [count])
 
   const ButtonClicked = () => {
     if (!checkBox) {
       window.alert("利用規約に同意してください");
     } else if(!checkPolicy) {
-      window.alert("利用規約をちゃんと読んでください");
+      window.alert("利用規約をちゃんと読んでから同意してください");
 
     } else {
       sessionStorage.setItem("name", `${name}`);
@@ -123,61 +119,9 @@ const Login: React.FC = () => {
                       ログイン
                     </button>
 
-                    <button
-                      className="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
-                      onClick={() => {setCount(count + 1)}}
-                    >
-                      キャンセル
-                    </button>
-                    <br/>
-                    {/*
-                    <CancelModal
-                      isCancelOpen={isCancelOpen}
-                      setCancelOpen={setCancelOpen}
-                    />
-                    */}
+                    
 
-{isCancelOpen ? (
-        <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                  本当にキャンセルしますか？
-                  </h3>
-                  
-                </div>
-                {/*body*/}
-                
-                {/*footer*/}
-                <div className="flex items-center justify-center p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => {setCancelOpen(false); setName(""); setEmail("") }}
-                    style={{minWidth: 100}}
-                  >
-                    はい
-                  </button>
-                  <button
-                    className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setCancelOpen(false)}
-                  >
-                    キャンセル
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-        </>
-      ) : null}
+
 
 
 
